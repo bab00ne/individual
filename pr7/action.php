207244401db61e9cc6c8de5f94c67a24fdc050cd
bab00ne <yurinikiforov8@gmail.com>
2023-10-07 11:31:09 +0500

    чето сделала

--- pr7/action.php
+++ pr7/action.php
@@ -1,19 +1,49 @@
 
 <?php 
+ $BackLink = '<a href="index.php"> К форме </a>';
+    if ( $_SERVER['REQUEST_METHOD'] !== 'POST' ) exit ('Метод запроса не POST'. '<BR>' . $BackLink);
+    if (empty($_POST['email'])) exit('Поле "Почта" не заполнено'. '<BR>' . $BackLink);
+    if (empty($_POST['name'])) exit('Поле "Логин" не заполнено'. '<BR>' . $BackLink);
+    if (empty($_POST['password'])) exit('Поле "Пароль" не заполнено'. '<BR>' . $BackLink);
+    if (empty($_POST['confirm_password'])) exit('Поле "Подтвердите пароль" не заполнено'. '<BR>' . $BackLink);
 
-    if ( $_SERVER['REQUEST_METHOD'] !== 'POST' ) exit ('Метод запроса не POST');  
-    if (empty($_POST['mail'])) exit('Поле "Почта" не заполнено');
-    if (empty($_POST['login'])) exit('Поле "Логин" не заполнено');
-    if (empty($_POST['password'])) exit('Поле "Пароль" не заполнено');
-    if (empty($_POST['password_repeat'])) exit('Поле "Подтвердите пароль" не заполнено');
+    if (strlen($_POST['password']) < 7) exit ('Поле "Пароль" должно содержать минимум 8 символов' . '<BR>' . $BackLink);
 
-    // if ($_POST['password'] < 8) exit ('Поле "Пароль" должно содержать минимум 8 соимволов');
-    // if ($_POST['password_repeat'] < 8) exit ('Поле "Подтвердите пароль" должно содержать минимум 8 соимволов');
+    if (strlen($_POST['password']) > 20) exit ('Поле "Пароль" должно содержать не более 20 символов' . '<BR>' . $BackLink);
 
-    // if ($_POST['password'] > 20) exit ('Поле "Пароль" должно содержать не более 20 соимволов');
-    // if ($_POST['password_repeat'] > 20) exit ('Поле "Подтвердите пароль" должно содержать не более 20 соимволов');
+    if ($_POST['password'] !== $_POST['confirm_password'] ) exit ('<b> Пароли не совпадают </b> !!');
 
-    if ($_POST['password'] !== $_POST['password_repeat'] ) exit ('<b> Пароли не совпадают </b> !!');
 
-echo 'Вы успешно зарегистрировались!';
-    ?>
+
+
+
+if (!isset ($_POST['email']) ) {
+    if (!isset ($_POST['name']) || (!isset ($_POST['password'])) || (!isset ($_POST['confirm_password']))) {
+        exit ('Во время регистрации произошла ошибка, проверьте ввведенные данные');
+    }
+}
+
+$connection = new PDO ('mysql:host=db4.myarena.ru;dbname=u19978_b10;charset=utf8', 'u19978_b10', 'yE1fQ3kE2c');
+
+$data = [$_POST['name'], $_POST['password'], $_POST['email']];
+
+$query = "SELECT * from User WHERE username=?";
+                   $result = mysqli_query($connection, $query) or die
+                       (mysqli_error($connection));
+
+if(mysqli_num_rows($result)> 0)
+    {
+        $multi = "Sorry ! This Username is not available...Please choose another";
+    }
+        else{ 
+
+
+$res = $connection->prepare("INSERT INTO regisration_form (username, password, email) VALUES (?, ?, ?)");
+$res = $res->execute ($data);
+                  }
+if ($res) {
+    exit ('Вы успешно зарегистрировались!'. '<BR>' . $BackLink);
+}
+exit ('Во время регистрации произошла ошибка, проверьте ввведенные данные'. '<BR>' . $BackLink);
+
+?>
--- pr7/index.php
+++ pr7/index.php
@@ -8,12 +8,11 @@
     <title>Форма</title>
 </head>
 <body>
-    <form action="action.php" ?data[role]=moderator&data [action]=work&m
-ood=super method="post"> <b>
-<p> Почта <input type="email" name="mail" required></p>
-<p> Логин <input type="text" name="login" required></p>
+    <form action="action.php" ?data[role]=moderator&data [action]=work&mood=super method="post"> <b>
+<p> Почта <input type="email" name="email" required></p>
+<p> Логин <input type="text" name="name" required></p>
 <p> Пароль <input type="password" name="password" required min ="8" max="20"></p>
-<p> Подтвердите пароль <input type="password" name="password_repeat" required min ="8" max="20"></p>
+<p> Подтвердите пароль <input type="password" name="confirm_password" required min ="8" max="20"></p>
 <p> Зарегистрироваться <input type="submit">
 <p> <input name="reset" type="reset" value="Очистить поля"> </p>
 </b>  </form>
