7a52e5584d3a3136df8b9ab4757c144c3fc1d961
bab00ne <yurinikiforov8@gmail.com>
2023-10-14 11:19:19 +0500

    oogpodgfpodgf

---
+++ Практика 3.2/script.js
@@ -0,0 +1,7 @@
+let a = prompt("Введите первое число");
+let b = prompt("Введите второе число");
+let big = a > b;
+let small = a < b; 
+let equal = a == b;
+let notequal = a != b;
+alert('Больше ли первое число: ' + big + "\n" + 'Меньше ли первое число: ' + small + "\n" + 'Равны ли числа: ' + equal + "\n" + 'Неравны ли числа: ' + notequal)
---
+++ Практика 5.1/script.js
@@ -0,0 +1,9 @@
+let login = prompt('Введите логин:');
+if (login = 'Админ') {
+    let pass = prompt('Введите пароль');
+    if (pass = 'Я главный') {alert('Здравствуйте!')}
+else alert ('Неверный пароль');
+    }
+ else if (login = null || e.KeyCode == 27) {
+    alert('Отменено');
+} else { alert}
